d73465e9e57f8d91fd1aa3b4fce04da91ad6f1ef
Ilion Beyst <ilion.beyst@gmail.com>
2020-05-25 15:04:33 +0200

    implement environment

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,42 +1,81 @@
 use super::parser::Expr;
 
-pub fn eval(expr: &Expr) -> Expr {
+use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Clone)]
+pub enum Value {
+    PrimitiveFn(&'static dyn Fn(&[Value]) -> Result<Value, String>),
+    Number(f64),
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::PrimitiveFn(_) => write!(f, "<primitive fn>"),
+            Value::Number(num) => write!(f, "{}", num),
+        }
+    }
+}
+
+struct Env {
+    bindings: HashMap<String, Value>,
+}
+
+impl Env {
+    fn resolve<'a>(&'a self, name: &str) -> Option<&'a Value> {
+        self.bindings.get(name)
+    }
+
+    fn default() -> Env {
+        let mut bindings: HashMap<String, Value> = HashMap::new();
+        bindings.insert("add".into(), Value::PrimitiveFn(&fun_add));
+        return Env { bindings };
+    }
+}
+
+pub fn eval(expr: &Expr) -> Result<Value, String> {
+    let env = Env::default();
+    return eval_(&env, expr);
+}
+
+fn eval_(env: &Env, expr: &Expr) -> Result<Value, String> {
     match expr {
-        Expr::Value(val) => Expr::Value(*val),
-        Expr::Identifier(id) => Expr::Identifier(id.clone()),
-        Expr::Sexp(exprs) => {
-            let vals = exprs.iter().map(eval).collect::<Vec<_>>();
-            return apply(vals);
-        }
-    }
-}
-
-fn apply(exprs: Vec<Expr>) -> Expr {
-    match &exprs[0] {
+        Expr::Value(num) => Ok(Value::Number(num.clone())),
         Expr::Identifier(id) => {
-            if let Some(res) = apply_fn(id, &exprs[1..]) {
-                return res;
+            match env.resolve(id) {
+                Some(val) => Ok(val.clone()),
+                None => Err(format!("unknown identifier {}", id))
             }
+        },
+        Expr::Sexp(exprs) => {
+            let mut evaluated = Vec::with_capacity(exprs.len());
+            for expr in exprs.iter() {
+                let value = eval_(env, expr)?;
+                evaluated.push(value);
+            }
+            apply(&evaluated)
         }
-        _ => ()
     }
-    return Expr::Sexp(exprs)
 }
 
-fn apply_fn(name: &str, args: &[Expr]) -> Option<Expr> {
-    let mut arg_values = Vec::with_capacity(args.len());
-    for arg in args.iter() {
-        if let Expr::Value(value) = arg {
-            arg_values.push(value);
+fn apply(values: &[Value]) -> Result<Value, String> {
+    match &values[0] {
+        Value::PrimitiveFn(fun) => {
+            fun(&values[1..])
+        }
+        _ => Err(format!("Type mismatch in apply"))
+    }
+}
+
+fn fun_add(values: &[Value]) -> Result<Value, String> {
+    let mut sum = 0.0;
+    for value in values {
+        if let Value::Number(num) = value {
+            sum += num;
         } else {
-            return None;
+            return Err("add: expected number".into());
         }
     }
-    match name {
-        "add" => {
-            let res = arg_values.into_iter().sum();
-            Some(Expr::Value(res))
-        }
-        _ => None,
-    }
+    return Ok(Value::Number(sum))
 }
--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let text = &args[1];
     match eval(text) {
-        Ok(res) => println!("{:?}", res),
+        Ok(res) => println!("{}", res),
         Err(err) => println!("Error: {}", err),
     }
 }
 
-fn eval(text: &str) -> Result<parser::Expr, String> {
+fn eval(text: &str) -> Result<evaluator::Value, String> {
     let tokens = lexer::lex_str(text)?;
     let expr = parser::parse_tokens(&tokens)?;
     let res = evaluator::eval(&expr);
-    return Ok(res);
+    return res;
 }
